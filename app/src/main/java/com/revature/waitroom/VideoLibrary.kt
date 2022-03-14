a32a19e839b78ed1f33c964e399803d0d59dd194
rnavarro97 <richard.navarro.1618@gmail.com>
2022-03-14 12:52:22 -0400

    Update VideoLibrary.kt

--- app/src/main/java/com/revature/waitroom/VideoLibrary.kt
+++ app/src/main/java/com/revature/waitroom/VideoLibrary.kt
@@ -3,10 +3,11 @@ package com.revature.waitroom
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,8 +32,17 @@ class VideoLibrary : ComponentActivity() {
 
 @Composable
 fun libraryPage() {
-    Text(text = "You've made quite a non-binary fuss" +
-            "to prevent intercontinental ballistic tweets.")
+    TopAppBar(title={Text("How-to Video Library")},
+        navigationIcon = {
+            IconButton(onClick = { /*TODO*/ })
+                {
+                Icon(Icons.Filled.Menu, contentDescription=null)
+                }
+        }
+    )
+    Column{
+        
+    }
 }
 
 @Preview(showBackground = true)
