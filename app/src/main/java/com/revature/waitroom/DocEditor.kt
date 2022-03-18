b4cb7dab0cd540adec45d3a4b2f2171ed2a79c13
Levi21333 <mholt2338@gmail.com>
2022-03-18 10:39:00 -0400

    Micah's Other Commit

--- app/src/main/java/com/revature/waitroom/DocEditor.kt
+++ app/src/main/java/com/revature/waitroom/DocEditor.kt
@@ -5,32 +5,49 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.revature.waitroom.data.InfoCards
+import com.revature.waitroom.R
 import com.revature.waitroom.ui.theme.WaitRoomTheme
 
 class DocEditor : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            WaitRoomTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    DocCard(name = "Micah Holt",
+                        description = "He has a common cold." +
+                                "He also have a headache and needs to take DayQuil" +
+                                "and get plenty of rest")
+
+                }
+
+            }
 
         }
     }
 }
 
 @Composable
-fun DocCard() {
-    val context = LocalContext.current
-
+fun DocCard(name:String,description:String) {
+//    val context = LocalContext.current
+//    var patientList:List<InfoCards>
     Card (
         modifier = Modifier
             .padding(20.dp)
@@ -42,29 +59,56 @@ fun DocCard() {
     ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
             Column(Modifier.padding(8.dp)) {
-              var name by  rememberSaveable { mutableStateOf("")}
-                TextField(value = name,
-                    onValueChange = {name = it},
-                    textStyle =MaterialTheme.typography.h3,
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h2,
+                    color = MaterialTheme.colors.onSurface,
                     modifier = Modifier.padding(bottom = 8.dp),
-                    label = { Text(text = "Patient's name:")}
+                    fontSize = 20.sp
                 )
 
-                var description by  rememberSaveable { mutableStateOf("")}
-                TextField(value = description,
-                    onValueChange = {description = it},
-                    textStyle =MaterialTheme.typography.body2,
+                Text(text =description,
+                    style = MaterialTheme.typography.body2,
+                    color = MaterialTheme.colors.onSurface,
                     modifier = Modifier.padding(bottom = 4.dp),
-                    label = { Text(text = "Diagnosis:")},
-                    maxLines = 4
+                    fontSize = 12.sp
                 )
+
+//                var name by  rememberSaveable { mutableStateOf("")}
+//                TextField(value = name,
+//                    onValueChange = {name = it},
+//                    textStyle =MaterialTheme.typography.h3,
+//                    modifier = Modifier.padding(bottom = 8.dp),
+//                    label = { Text(text = "Patient's name:")}
+//                )
+
+//                var description by  rememberSaveable { mutableStateOf("")}
+//                TextField(value = description,
+//                    onValueChange = {description = it},
+//                    textStyle =MaterialTheme.typography.body2,
+//                    modifier = Modifier.padding(bottom = 4.dp),
+//                    label = { Text(text = "Diagnosis:")},
+//                    maxLines = 4
+//                )
             }
         }
 
     }
+}
 
 
+//
+@Composable
+fun PatientPrompt(patientList:List<InfoCards>){
+    Scaffold(
+        topBar = {
+            TopAppBar() {
 
+            }
+        }
+    ) {
 
+    }
+    
 }
 
